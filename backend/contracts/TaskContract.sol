4a37c2e0f58615fb5c6ea9e2c202505016b01220
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2022-08-28 01:54:17 +0800

    Update contract

--- backend/contracts/TaskContract.sol
+++ backend/contracts/TaskContract.sol
@@ -25,7 +25,28 @@ contract TaskContract {
  }
 
   function getMyTasks () external view returns (Task[] memory) {
-    
+    Task[] memory temporary = new Task[](tasks.length);
+    uint counter = 0;
+
+    for(uint i=0; i<tasks.length;i++ ){
+      if(taskToOwner[i] == msg.sender && tasks[i].isDeleted == false){
+        temporary[counter] = tasks[i];
+        counter++;
+      }
+    }
+
+    Task[] memory result = new Task [](counter);
+    for (uint i=0; i<counter; i++){
+      result[i]=temporary[i];
+    }
+    return result;
+  }
+
+  function deleteTask (uint taskId, bool isDeleted) external {
+  if (taskToOwner[taskId] == msg.sender){
+    tasks[taskId].isDeleted = isDeleted;
+    emit DeleteTask(taskId, isDeleted);
+  }
   }
 
 }
