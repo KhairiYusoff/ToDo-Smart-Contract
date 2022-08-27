8b3aa9528764d29a5b7d81d8ac846422db9a7221
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2022-08-28 01:31:09 +0800

    Update

--- backend/contracts/TaskContract.sol
+++ backend/contracts/TaskContract.sol
@@ -3,6 +3,29 @@ pragma solidity 0.8.15;
 
 
 contract TaskContract {
+
   event AddTask (address recipient, uint taskId);
   event DeleteTask (uint taskId, bool isDeleted);
+
+  struct Task {
+    uint id;
+    string taskText;
+    bool isDeleted;
+  }
+
+ Task[] private tasks;
+
+ mapping (uint256 => address) taskToOwner;
+
+ function addTask (string memory taskText, bool isDeleted) external {
+  uint taskId = tasks.length;
+  tasks.push(Task(id, taskText, isDeleted));
+  taskToOwner[taskId] = msg.sender;
+  emit AddTask(msg.sender, taskId);
+ }
+
+  function getMyTasks () external view returns (Task[] memory) {
+    
+  }
+
 }
