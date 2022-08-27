65ec4feebf48d8c055ec16348bc273744278726a
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2022-08-28 01:03:14 +0800

    first commit

---
+++ backend/contracts/TaskContract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+
+contract TaskContract {
+  event AddTask (address recipient, uint taskId);
+  event DeleteTask (uint taskId, bool isDeleted);
+}
