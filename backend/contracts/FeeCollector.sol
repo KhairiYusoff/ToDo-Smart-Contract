e519b385ca58356f9729d9474fe028c8760a84be
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2022-08-31 00:06:48 +0800

    update contract

--- backend/contracts/FeeCollector.sol
+++ backend/contracts/FeeCollector.sol
@@ -3,8 +3,20 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract FeeCollector {
     address public owner;
+    uint256 public balance;
 
     constructor() {
         owner = msg.sender;
+    }
+
+    receive() external payable {
+        balance += msg.value;
+    }
+
+    function withdraw(uint amount, address destAdrr) public {
+        require(msg.sender == owner, "Only owner can withdraw");
+        require(amount <= balance, "Insufficient funds");
+        destAdrr.transfer(amount);
+        balance -= amount;
     }
 }
